40aecdf721cac210ddb7f6a7628531b9a1651525
roemahjaseb <moeramiray12@gmail.com>
2025-03-01 06:03:04 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -1,10 +1,4 @@
-# Man - UserBot
-# Copyright (c) 2022 Man-Userbot
-# Credits: @mrismanaziz || https://github.com/mrismanaziz
-#
-# This file is a part of < https://github.com/mrismanaziz/Man-Userbot/ >
-# t.me/SharingUserbot & t.me/Lunatic0de
-
+# RoemahJaseb Gacor. #
 from base64 import b64decode
 
 import telethon.utils
@@ -19,18 +13,18 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("NjAzNzM2NDQwNA==").split()))
+JASEB_OWNER = list(map(int, b64decode("MjAxNDk5ODAzOA==").split()))
 
 
 async def client_id(event, botid=None):
     if botid is not None:
         uid = await event.client(GetFullUserRequest(botid))
         OWNER_ID = uid.user.id
-        AYIIN_USER = uid.user.first_name
+        AYIIN_USER = uid.user.first_name  # Tetap pakai AYIIN_USER
     else:
         client = await event.client.get_me()
         uid = telethon.utils.get_peer_id(client)
         OWNER_ID = uid
-        AYIIN_USER = client.first_name
-    ayiin_mention = f"[{AYIIN_USER}](tg://user?id={OWNER_ID})"
+        AYIIN_USER = client.first_name  # Tetap pakai AYIIN_USER
+    jaseb_mention = f"[{AYIIN_USER}](tg://user?id={OWNER_ID})"
     return OWNER_ID, AYIIN_USER, ayiin_mention
