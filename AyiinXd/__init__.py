80d9cec333ec1a41665f73b6ce0c9580d0ebc64f
roemahjaseb <moeramiray12@gmail.com>
2025-03-01 08:58:45 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -1,12 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# inline credit @keselekpermen69
-# Recode by @mrismanaziz
-# t.me/SharingUserbot
-#
+# repack by blue. #
 """ Userbot initialization. """
 
 import logging
