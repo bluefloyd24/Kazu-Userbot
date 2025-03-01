510dcce5cc67622022ad1fc49cc77a20c9e7c087
roemahjaseb <moeramiray12@gmail.com>
2025-03-01 19:45:43 +0700

    Update startup.py
    Signed-off-by: roemahjaseb <moeramiray12@gmail.com>

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -49,37 +49,32 @@ def multiayiin():
         sys.exit(1)
 
     if STRING_SESSION:
-    try:
-        bot.start()
-        LOOP.run_until_complete(ajg())
-        LOOP.run_until_complete(ayiin_client(bot))  # Ganti ke ayiin_client
-        user = bot.get_me()
-        name = user.first_name
-        uid = user.id
-        LOGS.info(
-            f"STRING_SESSION detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——"
-        )
-        if user.id in blacklistayiin:
-            LOGS.warning(MSG_BLACKLIST.format(version))
-            sys.exit(1)
-    except Exception as e:
-        LOGS.error(f"Error saat menjalankan bot: {str(e)}")
+        try:
+            bot.start()
+            LOOP.run_until_complete(ajg())
+            LOOP.run_until_complete(ayiin_client(bot))  # Ganti ke ayiin_client
+            user = bot.get_me()
+            name = user.first_name
+            uid = user.id
+            LOGS.info(
+                f"STRING_SESSION detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——"
+            )
+            if user.id in blacklistayiin:
+                LOGS.warning(MSG_BLACKLIST.format(version))
+                sys.exit(1)
+        except Exception as e:
+            LOGS.error(f"Error saat menjalankan bot: {str(e)}")
+
     if BOT_TOKEN:
         try:
-    bot.start()
-    LOOP.run_until_complete(ajg())
-    LOOP.run_until_complete(ayiin_client(bot))  # Ganti ke ayiin_client
-    user = bot.get_me()
-    name = user.first_name
-    uid = user.id
-    LOGS.info(
-        f"STRING_SESSION detected!\n┌ First Name: {name}\n└ User ID: {uid}\n——"
-    )
-    if user.id in blacklistayiin:
-        LOGS.warning(MSG_BLACKLIST.format(version))
-        sys.exit(1)
+            user = tgbot.get_me()
+            name = user.first_name
+            uname = user.username
+            LOGS.info(
+                f"BOT_TOKEN detected!\n┌ First Name: {name}\n└ Username: @{uname}\n——"
+            )
+        except Exception as e:
+            LOGS.error(f"Error saat mengakses BOT_TOKEN: {str(e)}")
 
-except Exception as e:
-    LOGS.error(f"Error saat menjalankan bot: {str(e)}")
     if not STRING_SESSION:
         LOGS.warning("STRING_SESSION tidak ditemukan! Harap periksa kembali.")
