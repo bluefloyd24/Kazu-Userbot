fa5b86b6b54314a50d00e6cbcb4d4cdd51d85bc7
roemahjaseb <moeramiray12@gmail.com>
2025-03-01 14:23:12 +0700

    Update yinsfont.py
    Signed-off-by: roemahjaseb <moeramiray12@gmail.com>

--- AyiinXd/modules/yinsfont.py
+++ AyiinXd/modules/yinsfont.py
@@ -595,10 +595,13 @@ async def fonts(yins):
  "• bold         > **𝗿𝗼𝗲𝗺𝗮𝗵𝗷𝗮𝘀𝗲𝗯**\n"
  "• bolditalic   > **𝙧𝙤𝙚𝙢𝙖𝙝𝙟𝙖𝙨𝙚𝙗**\n\n"
 "**𝗥𝗢𝗘𝗠𝗔𝗛𝗝𝗔𝗦𝗘𝗕-userbot!**"
-CMD_HELP.update({
-    "yinsfont": f"**Plugin :** `roemahjasebfont`\n\n"
-                f"  »  **Perintah :** `{cmd}font` `<nama font>` `<teks/balas ke pesan>`\n"
-                f"  »  **Kegunaan :** Membuat Text dengan Fonts Style.\n\n"
-                f"  »  **Perintah :** `{cmd}lf`\n"
-                f"  »  **Kegunaan :** Untuk Melihat Daftar Font."
-})
+CMD_HELP.update(
+    {
+    "yinsfont": f"**Plugin : **`rjbfont`\
+        \n\n  »  **Perintah :** `{cmd}font` `<nama font>` `<teks/balas ke pesan>`\
+        \n  »  **Kegunaan : **Membuat Text dengan Fonts Style.\
+        \n\n  »  **Perintah :** `{cmd}lf`\
+        \n  »  **Kegunaan : **Untuk Melihat Daftar Font.\
+    "
+    }
+)
