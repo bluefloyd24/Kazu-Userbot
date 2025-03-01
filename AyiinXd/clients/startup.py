8e8351b821657632cb3d1c881a7b19bc6ea23ef6
roemahjaseb <moeramiray12@gmail.com>
2025-03-01 15:28:27 +0700

    Update startup.py
    Signed-off-by: roemahjaseb <moeramiray12@gmail.com>

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -23,8 +23,8 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nBluefloyd-Userbot v{} Copyright © 2021-2022 BLUEFLOYDUSERBOT• <https://github.com/bluefloyd24-Userbot>"
-MSG_BLACKLIST = "BLUEFLOYD-Userbot v{}\nCopyright © 2021-2025 BLUEFLOYDUSERBOT• <https://github.com/bluefloyd24/Kazu-Userbot>"
+EOL = "EOL\nRoemahjaseb-Userbot v{} Copyright © 2021-2025 RoemahjasebUSERBOT• <https://github.com/roemahjaseb/Kazu-Userbot>"
+MSG_BLACKLIST = "Roemahjaseb-Userbot v{}\nCopyright © 2021-2025 RoemahjasebUSERBOT• <https://github.com/roemahjaseb/Kazu-Userbot>"
 
 
 async def ayiin_client(client):
